68c0b817be8451f88b1c3e59213df9c7ba485574
vianey.jean <vianey.jean@ymail.com>
2023-08-03 21:12:22 +0400

    Finalisation du projet

--- src/components/TimerDisplay/timerdisplay.js
+++ src/components/TimerDisplay/timerdisplay.js
@@ -50,7 +50,7 @@ const TimerDisplay = ({ timerMode,
   }
 
   let timesUpMsg = timerMode === 'pomo'
-                  ? 'Temps pour une Pause'
+                  ? 'En Pause'
                   : 'Fin du Pause !'
 
   let timeText = timeLeft === '0:00'
